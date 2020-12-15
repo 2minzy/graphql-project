a1a22de292c6e1ef28d7a37dddb97be81a98b6cc
Minji <minjilee0423@gmail.com>
2020-12-15 23:11:00 +0900

    Added Home, Detail, Movie, apollo.js

--- graphql/db.js
+++ graphql/db.js
@@ -1,64 +1,45 @@
-export let movies = [
-  {
-    id: 0,
-    name: 'Avengers',
-    score: 6,
-  },
-  {
-    id: 1,
-    name: 'Logan',
-    score: 8,
-  },
-  {
-    id: 2,
-    name: 'Friends',
-    score: 9,
-  },
-  {
-    id: 3,
-    name: 'Interstella',
-    score: 10,
-  },
-  {
-    id: 4,
-    name: 'Darknight',
-    score: 7,
-  },
-  {
-    id: 5,
-    name: 'Grinch',
-    score: 4,
-  },
-  {
-    id: 6,
-    name: 'Once',
-    score: 9,
-  },
-];
+import axios from 'axios';
+const BASE_URL = 'https://yts-proxy.now.sh/';
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
 };
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
 };
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMoive;
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
 };
